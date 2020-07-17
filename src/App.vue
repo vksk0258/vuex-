0ab1939dd0a524da5aefe5c980fdf1f5d4da772e
vksk0258 <vksk0258>
2020-07-17 11:26:05 +0900

    app기본

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container"> <!--부트스트랩 클래스-->
+    <h1 class="text-center">Todo App</h1>
+    <input 
+    v-model='todoText'
+    type="text" 
+    class="w-100 p-2" 
+    placeholder="Type todo" 
+    @keyup.enter="addTodo" >
+    <hr>
+    <Todo 
+    v-for="todo in todos" 
+    :key="todo.id" 
+    :todo="todo"
+    @toggle-checkbox="toggleCheckbox"
+    @click-delete="deleteTodo"
+    /> <!--컴포넌트 하나를 key값에 따라 하나씩 넣어준다-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from '@/components/Todo.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Todo
+  },
+
+  data(){
+    return {
+      todoText: '',
+      todos: [
+        {id:1, text:"buy a car", checked :false},
+        {id:2, text:"play game", checked :false},
+      ]
+    }
+  },
+
+  methods: {
+    deleteTodo(id) {
+      // const index = this.todos.findIndex(todo => {
+      //   return todo.id === id;
+      // });
+      // this.todos[index].splice(index,1);
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    addTodo(e) { // e.target.value 이것으 이벤트로 받은 값을 얻을 수 있다
+      this.todos.push ({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false,
+      });
+      this.todoText = ''
+    },
+    toggleCheckbox({id, checked}) { //구조분해할당
+      const index = this.todos.findIndex(todo => { // 만약 toggleCheckbox에서 보낸 id랑 todos에 있는 아이디가 같다면 그 인덱스를 저장한다
+        return todo.id === id;                     // 첫번째 같은경우 처음 보낸 id는 1이고 todos첫번째 항복의 id도 1이기때문에 인덱스 0을 저장한다
+      });
+      this.todos[index].checked = checked;          //찾은 인덱스로 todos의 항목을 찾은 다음 해당 항목의 checked를 업데이트해준다
+      
+    },
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="mb-2 d-flex">
+      <div>
+        <input type="checkbox" :checked="todo.checked"
+        @change="toggleCheckbox">
+      </div>
+      <!-- todo.checked가 true면 text-muted를 해주고 아니면 아무것도 안한다
+      true면 text에 밑줄을 그어주고 아니면 아무것도 안한다 -->
+      <span 
+        class="ml-3 flex-grow-1"
+        :class="todo.checked ? 'text-muted' : ''"
+        :style="todo.checked ? 'text-decoration: line-through' : ''"
+        >
+        {{todo.text}}
+        </span>
+        <button 
+            class="btn btn-danger btn-sm"
+            @click='clickDelete'    
+            
+        >Delete</button>
+    </div> 
+</template>
+
+<script>
+export default {
+    props: {
+        todo: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        toggleCheckbox(e) {
+            console.log('hello')
+            this.$emit('toggle-checkbox', {
+                id: this.todo.id,
+                checked: e.target.checked
+
+            })
+        },
+        clickDelete() {
+            this.$emit('click-delete', this.todo.id);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
